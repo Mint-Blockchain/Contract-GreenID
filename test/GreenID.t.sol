3164c30239b2e6a24eb99966e28c5b06a77509c8
michael2024123 <michael@mintswap.finance>
2024-05-13 18:15:43 +0800

    GreenID

--- test/GreenID.t.sol
+++ test/GreenID.t.sol
@@ -8,6 +8,7 @@ import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
 contract TestGreenID is Test {
     address constant CHEATCODE_ADDRESS = 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D;
+    address constant SOME_ADDRESS = 0x21cB920Bf98041CD33A68F7543114a98e420Da0B;
 
     address private proxy;
     GreenID private instance;
@@ -30,14 +31,35 @@ contract TestGreenID is Test {
     
     function testMint() public {
         vm.prank(CHEATCODE_ADDRESS);
-
         instance.mint(CHEATCODE_ADDRESS, 1);
         assertEq(instance.ownerOf(1), CHEATCODE_ADDRESS);
 
         vm.prank(CHEATCODE_ADDRESS);
+        vm.expectRevert(bytes("GreenID: one address can only own one token"));
+        instance.mint(CHEATCODE_ADDRESS, 2);
 
+        vm.prank(CHEATCODE_ADDRESS);
+        instance.mint(SOME_ADDRESS, 2);
+       
+        vm.prank(CHEATCODE_ADDRESS);
         instance.claim(1);
         assertTrue(instance.claimedTokens(1));
+
+        vm.prank(CHEATCODE_ADDRESS);
+        vm.expectRevert(bytes("GreenID: token has already been claimed"));
+        instance.claim(1);
+
+        vm.prank(CHEATCODE_ADDRESS);
+        vm.expectRevert(bytes("GreenID: incorrect token owner"));
+        instance.claim(2);
+
+        vm.prank(CHEATCODE_ADDRESS);
+        vm.expectRevert(bytes("GreenID: Soul Bound Token"));
+        instance.transferFrom(CHEATCODE_ADDRESS, SOME_ADDRESS, 1);
+
+        vm.prank(SOME_ADDRESS);
+        vm.expectRevert(bytes("GreenID: Soul Bound Token"));
+        instance.safeTransferFrom(SOME_ADDRESS, CHEATCODE_ADDRESS, 2);
     }
 
 }
